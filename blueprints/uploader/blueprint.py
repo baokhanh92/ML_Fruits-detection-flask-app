33b3028c622007cb98ae53704520c1d08e133096
Felix.B <felix.b@thewalkingnerds.com>
2020-01-06 01:05:52 +0700

    improve model

--- blueprints/uploader/blueprint.py
+++ blueprints/uploader/blueprint.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, send_from_directory
 from keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.mobilenet import preprocess_input
 from werkzeug import secure_filename
 import tensorflow as tf
 import numpy as np
@@ -7,13 +9,12 @@ import os
 
 UPLOAD_FOLDER = 'uploads'
 
-# model = tf.keras.models.load_model('models/model.h5')
-model = tf.keras.models.load_model('models/fruit.h5')
+model = tf.keras.models.load_model('models/model.h5')
 
 uploader = Blueprint('uploader', __name__)
 
 # IMAGE_SIZE = 100
-IMAGE_SIZE = 224
+IMAGE_SIZE = 192
 
 
 @uploader.route('/uploader', methods=['GET', 'POST'])
@@ -37,12 +38,18 @@ def upload():
 
 def classify(upload_image_path):
 
-    image = tf.io.read_file(upload_image_path)
-    image = tf.image.decode_jpeg(image, channels=3)
-    image = tf.image.resize(image, [IMAGE_SIZE, IMAGE_SIZE])
-    image /= 255.0  # normalize to [0,1] range
-    image = tf.reshape(image, (1, IMAGE_SIZE, IMAGE_SIZE, 3))
-    probability = model.predict(image)
+    # img = tf.io.read_file(upload_image_path)
+    # img = tf.image.decode_jpeg(image, channels=3)
+    img = image.load_img(
+        upload_image_path, target_size=(IMAGE_SIZE, IMAGE_SIZE))
+    img_array = image.img_to_array(img)
+    expanded_image_array = np.expand_dims(img_array, axis=0)
+    preprocessed_image = preprocess_input(
+        expanded_image_array)  # Preprocess the image
+    # image = tf.image.resize(image, [IMAGE_SIZE, IMAGE_SIZE])
+    # image /= 255.0  # normalize to [0,1] range
+    # image = tf.reshape(image, (1, IMAGE_SIZE, IMAGE_SIZE, 3))
+    probability = model.predict(preprocessed_image)
 
     if probability[0][0] > probability[0][1] and probability[0][0] > probability[0][2]:
         return {"apple": round((probability[0][0] * 100), 2)}
